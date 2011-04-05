987e1355bce32784d8bf48efa7988814cf924b01
AdrianadelValle90@gmail.com <AdrianadelValle90@gmail.com@b215dc33-a66e-b3a8-656a-e99976d7edd2>
2011-04-05 18:35:30 +0000

    Creados los paquetes modelo, vista y controlador ademas de algunas clases en el modelo.

---
+++ trunk/EstComidaRapida-McDongo/src/Modelo/Establecimiento.java
@@ -0,0 +1,5 @@
+package Modelo;
+
+public class Establecimiento extends Persona {
+
+}
---
+++ trunk/EstComidaRapida-McDongo/src/Modelo/Persona.java
@@ -0,0 +1,53 @@
+package Modelo;
+
+public class Persona {
+
+	private String nombre;
+	private	String direccion;
+	private String telefono;
+	private String tipo;
+	
+	public Persona() {
+		super();
+	}
+	
+	public Persona(String nombre, String direccion, String telefono, String tipo) {
+		super();
+		this.nombre = nombre;
+		this.direccion = direccion;
+		this.telefono = telefono;
+		this.tipo = tipo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+}
